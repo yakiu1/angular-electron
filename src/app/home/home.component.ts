b2e6f9fac4f65eb551d3b1b44b6e83566c0c3aca
洪振剛 Gon <gon@miniasp.com>
2020-09-25 19:00:36 +0800

    fix youtube api loaded

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,7 @@
-import { AfterContentInit, Component, OnInit, ViewChild } from '@angular/core';
+import { YtPlayerService } from './../shared/common/yt-player.service';
+import { AfterContentInit, Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { BehaviorSubject, Subject, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -8,35 +10,50 @@ import { Router } from '@angular/router';
 })
 export class HomeComponent implements OnInit, AfterContentInit {
 
+  isReadySubscription = new Subscription();
+  $isReadyVideo = new BehaviorSubject<boolean>(false);
   videoId = 'WBI_cOPTzPU'
   reframed: boolean;
   player: YT.Player;
 
-  constructor(router: Router) {
-    // this.version = NGYTPackage['dependencies']['ngx-youtube-player'].replace('^', '');
+  constructor(router: Router, private ytPlayerService: YtPlayerService) {
+
+  }
+
+  loadYoutubeAPI(): void {
+    const tag = document.createElement('script');
+
+    tag.src = "https://www.youtube.com/iframe_api";
+    const firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+    window['onYouTubeIframeAPIReady'] = () => {
+      this.$isReadyVideo.next(true);
+    }
+  }
+
+  addListeners(): void {
+
+    this.isReadySubscription = this.$isReadyVideo.subscribe(isReady => {
+      if (isReady) {
+        console.log('API Ready!');
+        this.player = this.ytPlayerService.startVideo(this.videoId);
+        // DoSometingWhenReady
+        // this.addListeners();
+      }
+    })
   }
 
   ngAfterContentInit(): void {
-    window['onYouTubeIframeAPIReady'] = () => this.startVideo();
+    this.addListeners();
+    this.loadYoutubeAPI();
+    // this.player.playVideo();
+    console.log('XD');
+
   }
 
   ngOnInit(): void {
 
   }
 
-  startVideo(): void {
-    this.reframed = false;
-    this.player = new window['YT'].Player('player', {
-      videoId: this.videoId,
-      events: {
-        'onReady': this.videoReady.bind(this),
-      }
-    });
 
-  }
-
-  videoReady(evt: any): void {
-
-    this.player.playVideo();
-  }
 }
